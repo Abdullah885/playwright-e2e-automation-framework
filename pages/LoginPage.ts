a53c195e29dac74482023ebae83904bf8ab4a2d1
Abdullah <abdullah.javed.0710@gmail.com>
2026-03-16 08:17:11 +0500

    Refactored LoginPage with locator architecture

--- pages/LoginPage.ts
+++ pages/LoginPage.ts
@@ -1,10 +1,20 @@
-import {Page} from '@playwright/test';
+import {Page, Locator   } from '@playwright/test';
 
 export class LoginPage {
     readonly page: Page;
+    readonly usernameInput: Locator;
+    readonly passwordInput: Locator;
+    readonly loginButton: Locator;
+
+
+
 
     constructor(page:Page){
         this.page = page;
+
+        this.usernameInput= page.locator('#user-name');
+        this.passwordInput= page.locator('#password');
+        this.loginButton= page.locator('#login-button');
 
     }
 
@@ -15,9 +25,9 @@ export class LoginPage {
 
     async login (username: string, password: string){
 
-        await this.page.fill('#user-name', username);
-        await this.page.fill('#password', password);
-        await this.page.click('#login-button');
+        await this.usernameInput.fill(username);
+        await this.passwordInput.fill(password);
+        await this.loginButton.click();
 
 
 
